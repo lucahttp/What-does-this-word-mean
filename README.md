ce92e87ded5ea144109f0d99380ad3197d4d84ee
lukaneco <32082555+lukaneco@users.noreply.github.com>
2020-05-17 18:36:04 -0300

    first

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # What does this word mean
- # Word mean app (urban diccionary)     based on:  speech to text example https://github.com/stacktipslab/Android-Speech-to-Text-Example   api: http://api.urbandictionary.com/v0/define?term=donald%20trump
+
+# Word mean app (urban diccionary)     
+
+based on:  
+
+speech to text example 
+https://github.com/stacktipslab/Android-Speech-to-Text-Example   
+
+
+api: 
+
+http://api.urbandictionary.com/v0/define?term=donald%20trump
---
+++ app/src/main/java/com/sainluca/whatdoesthiswordmean/MainActivity.java
@@ -0,0 +1,206 @@
+package com.sainluca.whatdoesthiswordmean;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+
+
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQ_CODE_SPEECH_INPUT = 100;
+    private TextView mVoiceInputTv;
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
+    private ImageButton mSpeakBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        mVoiceInputTv = (TextView) findViewById(R.id.voiceInput);
+        mSpeakBtn = (ImageButton) findViewById(R.id.btnSpeak);
+        mSpeakBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startVoiceInput();
+            }
+        });
+
+
+        TextView mTextViewResult = (TextView) findViewById(R.id.text_view_result);
+        RequestQueue mQueue = Volley.newRequestQueue(this);
+
+        /*
+        View buttonParse = findViewById(R.id.btnSpeak);
+        RequestQueue mQueue = Volley.newRequestQueue(this);
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Running function",Toast.LENGTH_SHORT).show();
+                getInfoFromApi3("moe");
+            }
+        });
+
+         */
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void startVoiceInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    mVoiceInputTv.setText(result.get(0));
+
+                    //added from me
+                    getInfoFromApi3(result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
+
+
+    private void getInfoFromApi(String word){
+        String info = null;
+
+        String url = "https://api.myjson.com/bins/kp9wz";
+
+        String link = "http://api.urbandictionary.com/v0/define?term="+word; //donald%20trump
+
+        Toast.makeText(getApplicationContext(),"searching in the api : "+link,Toast.LENGTH_SHORT).show();
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("employees");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject employee = jsonArray.getJSONObject(i);
+                                String firstName = employee.getString("firstname");
+                                int age = employee.getInt("age");
+                                String mail = employee.getString("mail");
+                                mTextViewResult.append(firstName + ", " + String.valueOf(age) + ", " + mail + "\n\n");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+    }
+
+
+    private void getInfoFromApi3(String word){
+
+        String link = "http://api.urbandictionary.com/v0/define?term="+word; //donald%20trump
+
+
+        final TextView textView = (TextView) findViewById(R.id.response);
+        // ...
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, link,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+}
